7b361c7ac3341e59006ee7f7a45be5c27cc2a9e1
Stephen Connor <stephen.m.connor@accenture.com>
2020-03-19 21:59:01 +0000

    Add goals form

---
+++ src/components/forms/goals-audit-form-control.js
@@ -0,0 +1,99 @@
+import util from 'util';
+import dedent from 'dedent';
+
+import Analytics from '../analytics'
+import FormControlBase from './form-control-base';
+import AccountsField from '../fields/accounts-field';
+import PropertiesField from '../fields/properties-field';
+import ProfilesField from '../fields/profiles-field';
+import SubmitButton from '../ui/submit-button';
+
+import constants from '../../config/constants';
+import events from '../../config/events';
+import ui from '../../config/ui';
+import { translate } from '../../utilities/translate'
+import db from '../../models/db';
+
+import AuditTable from '../audit-table';
+import Goal from '../../models/goal';
+
+const analytics = new Analytics(
+  process.env.CLIENT_ID
+  [process.env.SCOPES]
+);
+
+class GoalsAuditFormControl extends FormControlBase {
+  constructor() {
+    super();
+    this.analytics = analytics;
+    this.accounts = new AccountsField($('#ga-accounts'), this);
+    this.properties = new PropertiesField($('#ga-properties'), this);
+    this.profiles = new ProfilesField($('#ga-profiles'), this);
+
+    this.auditButton = new SubmitButton($('#audit-button'), this);
+
+    this.auditTable = new AuditTable(
+      'Goals',
+      'goal',
+      Goal,
+      'Audit Goals'
+    );
+
+    this.initFormSubmit();
+    this.initAuditButton();
+  }
+
+  initAuditButton() {
+    this.auditButton.on(events.BUTTONS.SUBMIT.CLICK, (event) => {
+      this.form.submit();
+    })
+
+    this.on(events.FIELDS.PROPERTIES.CHANGE, (event) => {
+      this.auditButton.toggle(event.elem);
+    });
+  }
+
+  initFormSubmit() {
+    this.form.on('submit', (event) => {
+      event.preventDefault();
+      const options = this.profiles.field.find('option:selected');
+
+      const headers = {
+        'excludeQueryParameters': 'Exclude URL Params'
+      }
+
+      this.listProfiles(options).then((data) => {
+        this.displayAuditTable(headers, data);
+      });
+
+    });
+  }
+
+  listProfiles(options) {
+    return Promise.all(
+      options.map(async (i, elem) => {
+        const item = $(elem).data('item');
+        return await this.analytics.listProfiles(item.accountId, item.webPropertyId).then((response) => {
+          return {
+            item: item,
+            objects: response
+          }
+        });
+      })
+    )
+  }
+
+  displayAuditTable(headers,data) {
+    this.modal.showModal(
+      'Audit Goals',
+      this.auditTable.generateTable(headers, data),
+      {
+        callback: (event) => {
+          console.log(event);
+        }
+      }
+    );
+  }
+}
+
+export default GoalsAuditFormControl;
--- src/pages/router.js
+++ src/pages/router.js
@@ -3,6 +3,7 @@ import customDimensionsUpload from './custom-dimensions/upload';
 import customDimensionsAudit from './custom-dimensions/audit';
 import customMetricsUpload from './custom-metrics/upload';
 import customMetricsAudit from './custom-metrics/audit';
+import goalsAudit from './goals/audit';
 import profilesAudit from './profiles/audit';
 
 const routes = {
@@ -11,6 +12,7 @@ const routes = {
   '/custom-dimensions/audit': customDimensionsAudit,
   '/custom-metrics/upload': customMetricsUpload,
   '/custom-metrics/audit': customMetricsAudit,
+  '/goals/audit': goalsAudit,
   '/profiles/audit': profilesAudit,
 }
 
