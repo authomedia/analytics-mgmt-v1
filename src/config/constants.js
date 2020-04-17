27cf05708997f4bf93e9ef6d33465996a35d275c
Sean Burton <sean@analyt.co.uk>
2020-04-17 15:44:25 +0100

    Addition of Custom Metrics

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,6 @@
 const constants = {
-  DB_FIELDS: ['index','name', 'scope', 'active']
+  DB_FIELDS: ['index', 'name', 'scope', 'active'],
+  DB_FIELDS2: ['index', 'name', 'scope', 'type', 'active']
 }
 
 export default constants;
--- src/pages/router.js
+++ src/pages/router.js
@@ -1,11 +1,16 @@
 import remarketing from './remarketing';
 import customDimensionsUpload from './custom-dimensions/upload';
 import customDimensionsAudit from './custom-dimensions/audit';
+import customMetricsUpload from './custom-metrics/upload';
+import customMetricsAudit from './custom-metrics/audit';
+
 
 const routes = {
   '/': remarketing,
   '/custom-dimensions/upload': customDimensionsUpload,
   '/custom-dimensions/audit': customDimensionsAudit,
+  '/custom-metrics/upload': customMetricsUpload,
+  '/custom-metrics/audit': customMetricsAudit,
 }
 
 export default routes;
